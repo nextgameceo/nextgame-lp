3988f4e10737fc613cc9bcb1e1de559c7b027577
HIROKI UCHIYAMA <nextgamecoo@gmail.com>
2026-04-18 02:06:16 +0900

    next.config.js を更新

--- next.config.js
+++ next.config.js
@@ -3,29 +3,50 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)', // ← 全ページ対象（ここ重要）
+        source: '/lp/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; img-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:; font-src 'self' data: https:; frame-src 'self' https:; frame-ancestors 'self' https://nextgame-limited.com https://www.nextgame-limited.com; connect-src 'self' https:;",
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob:;
-              img-src 'self' data: blob: https:;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:;
-              style-src 'self' 'unsafe-inline' https:;
-              font-src 'self' data: https:;
-              frame-src 'self' https:;
-              frame-ancestors 
-                'self'
-                https://nextgame-limited.com
-                https://www.nextgame-limited.com
-                https://nextgameceo.github.io
-                https://nextgame-lp.vercel.app;
-              connect-src 'self' https:;
-            `.replace(/\n/g, ''),
+            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: https:; img-src 'self' data: blob: https:; script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; style-src 'self' 'unsafe-inline' https:; font-src 'self' data: https:; frame-src 'self' https:; frame-ancestors 'self' https://nextgame-limited.com https://www.nextgame-limited.com; connect-src 'self' https:;",
+          },
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
           },
         ],
       },
     ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.microcms-assets.io' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
   },
 };
 
